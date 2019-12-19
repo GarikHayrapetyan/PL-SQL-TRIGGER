13bebdfe9817a989795f32d109361cf60cecb9bb
GarikHayrapetyan <44756744+GarikHayrapetyan@users.noreply.github.com>
2019-12-19 20:48:21 +0100

    Add files via upload

---
+++ PL SQL Triggers.sql
@@ -0,0 +1,78 @@
+--1
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER del_emp
+BEFORE DELETE ON emp FOR EACH ROW
+BEGIN
+  Raise_application_error(-20100,'Cannot remove employees!') ;   
+END;
+
+delete from emp where empno=7369;
+
+select * from emp;
+
+
+--2
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER upd_sal
+BEFORE INSERT OR UPDATE ON emp 
+FOR EACH ROW
+DECLARE
+ v_sal emp.sal%type;
+BEGIN
+  v_sal := :new.sal;
+  IF INSERTING THEN    
+    IF v_sal < 1000 THEN
+      Raise_application_error(-20100,'Salary too low!') ;   
+    END IF;
+  ELSIF UPDATING THEN
+    IF v_sal < 1000 THEN
+      :new.sal := :old.sal;
+      dbms_output.put_line('Salary too low!');         
+   END IF;
+  END IF;
+END;
+
+update emp set sal=500 where empno= 7369;
+
+--3
+CREATE TABLE budget (currentValue INT NOT NULL);
+
+INSERT INTO budget (currentValue)
+SELECT SUM(sal) FROM emp;
+
+
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER upd_budget
+AFTER INSERT OR UPDATE OR DELETE ON emp 
+BEGIN
+  UPDATE budget SET currentValue = (SELECT SUM(sal) FROM emp);
+END;
+
+--4
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE TRIGGER task_4
+BEFORE INSERT OR UPDATE OR DELETE ON emp 
+FOR EACH ROW
+DECLARE 
+    v_count INT;
+BEGIN 
+    IF DELETING AND :old.sal > 0 THEN     
+        Raise_application_error(-20100,'cannot remove employee with salary above 0'); 
+    ELSIF (UPDATING AND :old.ename != :new.ename) THEN 
+       -- Raise_application_error(-20100,'changing names is forbidden!');  
+        dbms_output.put_line('changing names is forbidden!');  
+    ELSIF INSERTING THEN    
+        SELECT COUNT(1) INTO v_count FROM emp e WHERE e.ename = :new.ename; 
+        IF v_count > 0 THEN
+            Raise_application_error(-20100,'Employee with this name already exists!');  
+        END IF;        
+    END IF;
+END;
+
+/* Tests
+DELETE FROM emp e WHERE empno = 7499;
+
+UPDATE emp set ename ='aa' WHERE empno =7499;
+
+INSERT INTO EMP (empno, ename) VALUES (1, 'ALLEN');
+*/
